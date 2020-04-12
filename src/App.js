d3eca1acacd8fe60a197e0deedf4cdd52d435e41
Louzada <silvalouzada@gmail.com>
2020-04-12 05:14:31 -0300

    Add CRUD Repositories (no-edit-repository)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const { data: response } = await api.post("repositories", {
+        title: "desafio-conceitos-reactjs",
+        url: "https://github.com/louzada01/gostack-desafio-conceitos-reactjs",
+        techs: ["ReactJs", "React", "ES6", "JavaScritp"],
+      });
+
+      setRepositories([...repositories, response]);
+    } catch (error) {
+      alert("Is not possible add repository - " + error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+      const newRepositories = repositories.filter(
+        repository => repository.id !== id
+      )
+      setRepositories(newRepositories)
+    } catch (error) {
+      alert(error);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((item) => (
+          <li key={item.id}>
+            {item.title}
+            <button onClick={() => handleRemoveRepository(item.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
